0893858d5cf8a4e4fbd501b1bd2c07da642d7c4a
Corey Mays <corey.mays17@gmail.com>
2024-09-19 18:10:57 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
-        
+        double springMain = 130.61;
+        double summerMain = 95.47;
+        double fallMain = 241.39;
+        double winterMain = 172.65;
+        double totalMain = springMain + summerMain + fallMain + winterMain;
+
+        System.out.println("The respective maintenance costs for the Spring, Summer, Fall, and Winter are " + springMain + ", " + summerMain + ", " + fallMain + ", " + winterMain + ". Which total to " + totalMain + " for the year.");
     }
 }
